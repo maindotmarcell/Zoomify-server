a07e39b911a364aca5f35743eb45a197151ea704
Marcell Munnich <marcellmunnich@gmail.com>
2022-11-06 20:24:56 +1100

    Google done (Raw version)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import session from 'express-session';
 import passport from 'passport';
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 dotenv.config();
 
@@ -26,9 +27,51 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.serializeUser((user: any, done) => {
+	return done(null, user);
+});
+
+passport.deserializeUser((user: any, done) => {
+	return done(null, user);
+});
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: `${process.env.GOOGLE_CLIENT_ID}`,
+			clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
+			callbackURL: '/auth/google/callback',
+		},
+		function (accessToken: any, refreshToken: any, profile: any, cb: any) {
+			// Called on Successful Authentication!
+			// Insert into Database
+			console.log(profile);
+			cb(null, profile);
+		}
+	)
+);
+
 app.get('/', (req, res) => {
 	res.send('Hello World');
 });
+
+app.get('/getuser', (req, res) => {
+	res.send(req.user);
+});
+
+app.get(
+	'/auth/google',
+	passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get(
+	'/auth/google/callback',
+	passport.authenticate('google', { failureRedirect: '/login' }),
+	function (req, res) {
+		// Successful authentication, redirect home.
+		res.redirect('http://localhost:3000');
+	}
+);
 
 app.listen(4000, () => {
 	console.log('Server started');
