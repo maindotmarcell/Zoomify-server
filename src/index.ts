3c25d32c5354a449adf2c09a660331393ca10791
Marcell Munnich <marcellmunnich@gmail.com>
2022-11-06 17:14:12 +1100

    set up backend structure

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import session from 'express-session';
+import passport from 'passport';
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(
+	`${process.env.START_MONGODB}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}${process.env.END_MONGODB}`,
+	{},
+	() => {
+		console.log('Connected to mongoose succesfully');
+	}
+);
+
+// Middleware
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(
+	session({ secret: 'secretcode', resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+app.listen(4000, () => {
+	console.log('Server started');
+});
