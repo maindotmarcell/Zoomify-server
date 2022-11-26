b710dfa9303d2fd10136390868dc34340396277e
Marcell Munnich <marcellmunnich@gmail.com>
2022-11-26 22:48:32 +0100

    Added v1 Vid Chat via WebRTC & Socket.io

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,22 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import session from 'express-session';
 import passport from 'passport';
+const app = express();
+import http from 'http';
+const server = http.createServer(app);
+import { Server } from 'socket.io';
 
 dotenv.config();
 
-const app = express();
+const io = new Server(server, {
+	cors: {
+		origin: `${process.env.CLIENT_URL}`,
+		methods: ['GET', 'POST'],
+	},
+});
 
 // Route imports
 import authRoute from './routes/auth';
-
 
 mongoose.connect(
 	`${process.env.START_MONGODB}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}${process.env.END_MONGODB}`,
@@ -32,15 +40,32 @@ app.use(passport.session());
 
 app.use('/auth', authRoute);
 
-app.get('/', (req, res) => {
-	res.send('Hello World');
-});
-
 app.get('/getuser', (req, res) => {
 	// console.log(req);
 	res.send(req.user);
 });
 
-app.listen(4000, () => {
+io.on('connection', (socket) => {
+	socket.emit('me', socket.id);
+	console.log(socket.id);
+
+	socket.on('disconnet', () => {
+		socket.broadcast.emit('callEnded');
+	});
+
+	socket.on('callUser', (data) => {
+		io.to(data.userToCall).emit('callUser', {
+			signal: data.signalData,
+			from: data.from,
+			name: data.name,
+		});
+	});
+
+	socket.on('answerCall', (data) => {
+		io.to(data.to).emit('callAccepted', data.signal);
+	});
+});
+
+server.listen(4000, () => {
 	console.log('Server started');
 });
